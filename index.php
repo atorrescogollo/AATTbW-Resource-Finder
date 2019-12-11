47233d240328cfa11abde3021a5ed9df34d8a747
Álvaro Torres Cogollo <atorrescogollo@gmail.com>
2019-12-11 12:59:36 +0100

    Basic Index Layout

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Práctica 3</title>
+	<link rel="shortcut icon" type="image/png" href="images/logo.png" />
+
+	<link href="css/aattbw.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+	<div id="zone1" class="dockbar">
+		<div id=logo-container class=container>
+			<img src=images/logo.png />
+		</div>
+		<div id=title class=container>
+			<h1>AATTbW</h1>
+		</div>
+		<div class="container login-container">
+			<form>
+				<label class="labelbox" for="login-box">Login</label>
+				<input type="text" name="login-box">
+				<label class="labelbox" for="password-box">Password</label>
+				<input type="password" name="password-box">
+				<button type="submit">Entrar</button>
+			</form>
+		</div>
+	</div>
+	<div id=datazones>
+		<div id=datazonesrow>
+			<div id="zone2-container" class="container scroller">
+				<div id=zone2 >
+					<p>Temporal</p>
+				</div>
+			</div>
+			<div id="zone3-container" class="container scroller">
+				<div id=zone3 >
+					<p>Temporal</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="zone4" class="dockbar">
+		<p>Temporal</p>
+	</div>
+</body>
+
+</html>
