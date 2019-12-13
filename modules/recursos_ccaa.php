99ae4d5f49bfcc6acba2ba14ddef1c98e348944c
Álvaro Torres Cogollo <atorrescogollo@gmail.com>
2019-12-13 22:51:50 +0100

    Added "CCAA resources" basic layout

--- modules/recursos_ccaa.php
+++ modules/recursos_ccaa.php
@@ -1,3 +1,16 @@
-<?php
-echo "Temporal";
-?>
+<h5 class=step-container-title>Paso 1</h5>
+<div id=step1 class=step-container>
+    <form>
+        <label for="filter">Filtro: </label>
+        <input name="filter"/>
+        <button type="submit">Buscar</button>
+    </form>
+</div>
+
+<h5 class=step-container-title>Paso 2</h5>
+<div id=step1 class=step-container>
+</div>
+
+<h5 class=step-container-title>Mapa</h5>
+<div id=map class=step-container>
+</div>
